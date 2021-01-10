5053f3f56be5966b29a2b28a562664f767e01006
djram <dj.ramirez1221@gmail.com>
2021-01-09 23:06:55 -0500

    changed ytsearch method to take input

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import os
-
+import requests
+import json
 import discord
 from discord.ext import commands
+import ytSearch
 from dotenv import load_dotenv
+
 load_dotenv('botToken.env')
 
 client = discord.Client()
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    data = json.loads(response.text)
+    quote = data[0]['q'] + " -" + data[0]['a']
+    return(quote)
+def get_query():
+    #hardcoded query
+    #will be changed in order to take any input
+    q = "whatsup"
+    response = ytSearch.youtube_query(q)
+    return(response)
 
 @client.event
 async def on_ready():
@@ -16,7 +31,12 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!henlo'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('t'):
+        quote = get_quote()
+        query = get_query()
+        await message.channel.send(query)
 
 client.run(os.getenv('TOKEN'))
+
+
+
--- ytSearch.py
+++ ytSearch.py
@@ -5,14 +5,16 @@
 # https://developers.google.com/explorer-help/guides/code_samples#python
 
 import os
-
+import json
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-scopes = ["https://www.googleapis.com/auth/youtube"]
 
+scopes = ["https://www.googleapis.com/auth/youtube"]
+'''
 def main():
+
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -28,15 +30,50 @@ def main():
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials)
 
+
+
+    #Seperate this into a method that can pass a custom query
+    
     request = youtube.search().list(
         part="snippet",
         channelType="any",
-        maxResults=1,
-        q="jujutsu kaisen op"
+        maxResults=3,
+        q= "query"
     )
     response = request.execute()
+    
+    
+    data = json.loads(response)
 
-    print(response)
+    return(data)
+''' 
 
+
+def youtube_query(query):
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "desktopClientSecret.json"
+
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    request = youtube.search().list(
+        part="snippet",
+        channelType="any",
+        maxResults=3,
+        q= query
+    )
+    response = request.execute()
+    data = json.loads(response)
+
+    return data
+
+
+'''
 if __name__ == "__main__":
     main()
+'''
